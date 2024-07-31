5fa1a57fa186b3a96088d38ca74a1ca824232377
Augusto <augustoalbarracin25487@gmail.com>
2024-07-31 02:26:47 -0300

    fix(transactions): Update empty-transactions component´s position and adjust text-alignment

--- src/infraestructure/router/router.tsx
+++ src/infraestructure/router/router.tsx
@@ -1,19 +1,14 @@
-import { FC } from 'react'
-import { BrowserRouter } from 'react-router-dom'
-import { PrivateRouter } from './private'
-import { PublicRouter } from './public'
-import { useAuthStore } from '../zustand/auth/useAuthStore';
+import { FC } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { PrivateRouter } from "./private";
+import { PublicRouter } from "./public";
+import { useAuthStore } from "../zustand/auth/useAuthStore";
 
 export const AppRouter: FC = () => {
-    const { authenticated } = useAuthStore();
-    return (
-        <BrowserRouter>
-            {
-                authenticated ?
-                    <PrivateRouter />
-                    :
-                    <PublicRouter />
-            }
-        </BrowserRouter>
-    )
-}
+  const { authenticated } = useAuthStore();
+  return (
+    <BrowserRouter>
+      {!authenticated ? <PrivateRouter /> : <PublicRouter />}
+    </BrowserRouter>
+  );
+};
