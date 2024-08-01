f25688ae035812993b73a1381d690ed18a9e7d14
AlejandroPedrosa <alejandro.pedrosa00@gmail.com>
2024-08-01 14:40:16 -0300

    hotfix(routing): remove negation from auth status value

--- src/infraestructure/router/router.tsx
+++ src/infraestructure/router/router.tsx
@@ -1,14 +1,14 @@
-import { FC } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { PrivateRouter } from "./private";
-import { PublicRouter } from "./public";
-import { useAuthStore } from "../zustand/auth/useAuthStore";
+import { FC } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { PrivateRouter } from './private'
+import { PublicRouter } from './public'
+import { useAuthStore } from '../zustand/auth/useAuthStore'
 
 export const AppRouter: FC = () => {
-  const { authenticated } = useAuthStore();
+  const { authenticated } = useAuthStore()
   return (
     <BrowserRouter>
-      {!authenticated ? <PrivateRouter /> : <PublicRouter />}
+      {authenticated ? <PrivateRouter /> : <PublicRouter />}
     </BrowserRouter>
-  );
-};
+  )
+}
