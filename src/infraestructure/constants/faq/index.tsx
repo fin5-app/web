c0006b9191b6965361a753326330adf34d507168
Luciano de la Rubia <lucianodelarubia@MacBook-Air-de-Luciano.local>
2024-08-07 18:07:06 -0300

    chore(faq): Update faq items to ES

--- src/infraestructure/constants/faq/index.tsx
+++ src/infraestructure/constants/faq/index.tsx
@@ -10,60 +10,67 @@ export type FAQItem = {
   icon: ReactElement;
 };
 
+// todo:
+// 1. Definir contenido real
+// 1.1. Que sea español
+
 const faqItems: FAQItem[] = [
   {
-    question: 'What is Linear?',
-    answer: 'Linear is a modern issue tracking tool for software development.',
+    question: '¿Qué es Fin 5?',
+    answer: 'Fin 5 es una aplicación financiera que te ayuda a organizar y controlar tus finanzas personales y familiares de manera intuitiva.',
     icon: <ChartLineIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'How to integrate GitHub with Linear?',
-    answer: 'You can integrate GitHub with Linear by going to Settings > Integrations > GitHub and following the setup instructions.',
+    question: '¿Cómo puedo empezar a usar Fin 5?',
+    answer: 'Puedes comenzar a usar Fin 5 registrándote en nuestra plataforma y disfrutando de un periodo de prueba gratuito de 14 días.',
     icon: <LightIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'What is 3D Secure 2.1?',
-    answer: '3D Secure 2.1 is an improved authentication protocol for online transactions, enhancing security and user experience.',
+    question: '¿Cómo administro mis ingresos y gastos?',
+    answer: 'Fin 5 te permite registrar y categorizar tus ingresos y gastos, brindándote una visión clara de tu situación financiera.',
     icon: <CreditCardIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'How do I reset my password?',
-    answer: 'To reset your password, click on the "Forgot Password" link on the login page and follow the instructions.',
+    question: '¿Puedo establecer metas financieras?',
+    answer: 'Sí, con Fin 5 puedes establecer metas financieras personalizadas y hacer seguimiento de tu progreso para alcanzarlas.',
     icon: <HandCoinsIcons style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'How do I change my email address?',
-    answer: 'To change your email address, go to your account settings and update your email information.',
+    question: '¿Es seguro usar Fin 5?',
+    answer: 'Sí, Fin 5 utiliza cifrado de nivel industrial para proteger tus datos y garantizar tu privacidad.',
     icon: <LightIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'Is my data secure?',
-    answer: 'Yes, we use industry-standard encryption to protect your data and ensure your privacy.',
+    question: '¿Puedo compartir mi cuenta con otros miembros de mi familia?',
+    answer: 'Sí, Fin 5 te permite compartir el acceso a tu cuenta con miembros de tu familia para que puedan colaborar en la gestión financiera.',
     icon: <LightIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'Can I get a refund?',
-    answer: 'Yes, we offer a 30-day money-back guarantee. If you are not satisfied with our service, you can request a refund within 30 days of purchase.',
+    question: '¿Cómo me contacto con soporte?',
+    answer: 'Puedes contactar a nuestro equipo de soporte a través de la página de soporte o enviando un correo electrónico a soporte@fin5.com.',
     icon: <LightIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'How do I contact support?',
-    answer: 'You can contact our support team via the support page or by emailing support@linear.com.',
+    question: '¿Cómo puedo cancelar mi suscripción?',
+    answer: 'Puedes cancelar tu suscripción en cualquier momento desde la configuración de tu cuenta. La cancelación se hará efectiva al final del periodo de facturación actual.',
     icon: <LightIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'What is the cancellation policy?',
-    answer: 'You can cancel your subscription at any time from your account settings. Cancellations take effect at the end of the current billing period.',
+    question: '¿Ofrecen descuentos?',
+    answer: 'Sí, ofrecemos descuentos para estudiantes, organizaciones sin fines de lucro y compras en grandes cantidades. Contacta a nuestro equipo de ventas para más información.',
     icon: <LightIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
   {
-    question: 'Are there any discounts available?',
-    answer: 'Yes, we offer discounts for students, non-profits, and bulk purchases. Please contact our sales team for more information.',
+    question: '¿Cómo puedo actualizar mis datos de contacto?',
+    answer: 'Para actualizar tus datos de contacto, ve a la configuración de tu cuenta y edita la información correspondiente.',
     icon: <LightIcon style={{ width: 40, height: 40 }} className='text-text-primary'/>,
   },
 ];
 
 export default faqItems;
+
+
+
 
 
 
