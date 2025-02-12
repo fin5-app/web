48dbc7c9e5ac365c5b601372d23170f9a5b76c9e
PedrosaIgnacio <ignaciopedrosa1998@gmail.com>
2025-02-12 17:38:57 -0300

    update layout styles

--- src/infraestructure/layouts/private-layout.tsx
+++ src/infraestructure/layouts/private-layout.tsx
@@ -11,7 +11,7 @@ export const PrivateLayout: FC<PrivateLayoutProps> = (props) => {
   return (
     <div className="flex flex-col lg:flex-row h-screen w-full">
       <Sidebar />
-      <div className="bg-primary-100 w-full flex justify-center py-5 lg:overflow-scroll">
+      <div className="bg-primary-100 w-full flex justify-center py-5 lg:overflow-auto">
         <div className="w-full lg:w-3/5 px-3 lg:px-0 ">
           <Breadcrumb />
           {children}
